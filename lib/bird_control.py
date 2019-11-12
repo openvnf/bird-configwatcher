c6927708ef2cb94d9fef3e24cadef74a7124f8d8
Jürgen Krutzler <juergen.krutzler@travelping.com>
2019-11-12 15:41:25 +0100

    Open/Close Socket to bird on each command

--- lib/bird_control.py
+++ lib/bird_control.py
@@ -3,18 +3,22 @@ class BirdControl:
 
     def __init__(self, socket_path):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.sock.connect(socket_path)
+        self.path = socket_path
 
     def reconfigure_check(self, config_path):
+        self.sock.connect(self.path)
         message = f'reconfigure check "{config_path}" \n'
         self.sock.sendall(message)
         reply = self.sock.recv(1024)
+        self.sock.close()
         return "Configuration OK" in reply
 
     def reconfigure(self, config_path):
+        self.sock.connect(self.path)
         message = f'reconfigure "{config_path}" \n'
         self.sock.sendall(message)
         reply = self.sock.recv(1024)
+        self.sock.close()
         return "Reconfigured" in reply
 
 
