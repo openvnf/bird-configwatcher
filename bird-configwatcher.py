1a7f9cb5f5e60a04894c10ed1a34cae5a2d46770
Jürgen Krutzler <juergen.krutzler@travelping.com>
2019-11-12 17:17:28 +0100

    rebuild inotify handling, configs are links, so check if hash of config changed, when inotify event happens

--- bird-configwatcher.py
+++ bird-configwatcher.py
@@ -2,7 +2,16 @@
 from lib import bird_control
 from inotify_simple import INotify, flags
 from prometheus_client import start_http_server, Enum
+import hashlib
 
+def sha256sum(filename):
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            h.update(mv[:n])
+    return h.hexdigest()
 
 def main():
     print('Starting prometheus exporter')
@@ -17,39 +26,43 @@ def main():
     bird4_prometheus = Enum('bird4_config_state', 'Bird IPv4 Config State', states=['valid', 'invalid'])
     bird6_prometheus = Enum('bird6_config_state', 'Bird IPv6 Config State', states=['valid', 'invalid'])
 
-    )
-    print()
-
     birdc_ip4 = bird_control.BirdControl(birdc_ip4_socket_path)
     birdc_ip6 = bird_control.BirdControl(birdc_ip6_socket_path)
+
+    bird_ip4_config_hash = sha256sum(birdc_ip4_config)
+    bird_ip6_config_hash = sha256sum(birdc_ip6_config)
+
     inotify = INotify()
     watch_flags = flags.CREATE | flags.MODIFY
     inotify.add_watch(bird_config_folder, watch_flags)
-    print(f'watching config {birdc_ip4_config} {birdc_ip6_config}'
-    for event in inotify.read():
-        print('got event from inotify')
-        full_path_event = f'{bird_config_folder}/{event.name}'
-        if full_path_event == birdc_ip4_config:
-            print('IPv4 config change')
-            valid_config = birdc_ip4.reconfigure_check(birdc_ip4_config)
-            if valid_config :
-                birdc_ip4.reconfigure(birdc_ip4_config)
-                bird4_prometheus.state('valid')
-                print('valid IPv4 Config')
-            else :
-                bird4_prometheus.state('invalid')
-                print('BAD IPv4 Config')
-        elif full_path_event == birdc_ip6_config:
-            print('IPv6 config change')
-            valid_config = birdc_ip6.reconfigure_check(birdc_ip6_config)
-            if valid_config :
-                birdc_ip6.reconfigure(birdc_ip6_config)
-                bird6_prometheus.state('valid')
-                print('valid IPv6 Config')
-            else :
-                bird6_prometheus.state('invalid')
-                print('BAD IPv6 Config')
-        else:
-            print('unknown file added, or changed')
+    print(f'watching config directory {bird_config_folder}')
+    while True:
+        for event in inotify.read():
+            print('got inotify event, checking what has changed')
+            new_bird_ip4_config_hash = sha256sum(birdc_ip4_config)
+            new_bird_ip6_config_hash = sha256sum(birdc_ip6_config)
+
+            if new_bird_ip4_config_hash != bird_ip4_config_hash:
+                print('IPv4 config change')
+                bird_ip4_config_hash = new_bird_ip4_config_hash
+                valid_config = birdc_ip4.reconfigure_check(birdc_ip4_config)
+                if valid_config :
+                    birdc_ip4.reconfigure(birdc_ip4_config)
+                    bird4_prometheus.state('valid')
+                    print('valid IPv4 Config')
+                else :
+                    bird4_prometheus.state('invalid')
+                    print('BAD IPv4 Config')
+            if new_bird_ip6_config_hash != bird_ip6_config_hash:
+                print('IPv6 config change')
+                bird_ip6_config_hash = new_bird_ip6_config_hash
+                valid_config = birdc_ip6.reconfigure_check(birdc_ip6_config)
+                if valid_config :
+                    birdc_ip6.reconfigure(birdc_ip6_config)
+                    bird6_prometheus.state('valid')
+                    print('valid IPv6 Config')
+                else :
+                    bird6_prometheus.state('invalid')
+                    print('BAD IPv6 Config')
 
 main()
