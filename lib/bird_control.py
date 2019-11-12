242f75b748ee03d907518b402cc74700322bf5a8
Jürgen Krutzler <juergen.krutzler@travelping.com>
2019-11-12 18:00:36 +0100

    fix bird syntax bug and add debugging output

--- lib/bird_control.py
+++ lib/bird_control.py
@@ -7,17 +7,21 @@ class BirdControl:
 
     def reconfigure_check(self, config_path):
         self.sock.connect(self.path)
-        message = f'reconfigure check "{config_path}" \n'
-        self.sock.sendall(bytes(message,'utf-8'))
+        message = bytes(f'configure check "{config_path}" \n','utf-8')
+        print(f'send message: {message}')
+        self.sock.sendall(message)
         reply = self.sock.recv(1024)
+        print(f'get reply: {reply}')
         self.sock.close()
         return b'Configuration OK' in reply
 
     def reconfigure(self, config_path):
         self.sock.connect(self.path)
-        message = f'reconfigure "{config_path}" \n'
-        self.sock.sendall(bytes(message,'utf-8'))
+        message = bytes(f'configure "{config_path}" \n','utf-8')
+        print(f'send message: {message}')
+        self.sock.sendall(message)
         reply = self.sock.recv(1024)
+        print(f'get reply: {reply}')
         self.sock.close()
         return b'Reconfigured' in reply
 
