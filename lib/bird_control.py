1ca4c8282a492f474991d42031ad8bf56185faa1
Jürgen Krutzler <juergen.krutzler@travelping.com>
2019-11-12 17:39:20 +0100

    change strings to bytestrings

--- lib/bird_control.py
+++ lib/bird_control.py
@@ -8,18 +8,18 @@ class BirdControl:
     def reconfigure_check(self, config_path):
         self.sock.connect(self.path)
         message = f'reconfigure check "{config_path}" \n'
-        self.sock.sendall(message)
+        self.sock.sendall(bytes(message,'utf-8'))
         reply = self.sock.recv(1024)
         self.sock.close()
-        return "Configuration OK" in reply
+        return b'Configuration OK' in reply
 
     def reconfigure(self, config_path):
         self.sock.connect(self.path)
         message = f'reconfigure "{config_path}" \n'
-        self.sock.sendall(message)
+        self.sock.sendall(bytes(message,'utf-8'))
         reply = self.sock.recv(1024)
         self.sock.close()
-        return "Reconfigured" in reply
+        return b'Reconfigured' in reply
 
 
 
