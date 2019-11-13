223816f81e1ffb0e74315ead4e65a3e015234a05
Jürgen Krutzler <juergen.krutzler@travelping.com>
2019-11-13 08:28:46 +0100

    remove socket creation from init method and read from socket until 3 lines are received

--- lib/bird_control.py
+++ lib/bird_control.py
@@ -2,27 +2,35 @@ import socket
 class BirdControl:
 
     def __init__(self, socket_path):
-        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.path = socket_path
 
+    def read_lines(self, count, sock):
+        data = b''
+        while not data.count('\n') == count:
+            data += sock.recv(1024)
+        return data
+
     def reconfigure_check(self, config_path):
-        self.sock.connect(self.path)
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(self.path)
         message = bytes(f'configure check "{config_path}" \n','utf-8')
         print(f'send message: {message}')
-        self.sock.sendall(message)
-        reply = self.sock.recv(1024)
+        sock.send(message)
+        reply = self.read_lines(3, sock)
         print(f'get reply: {reply}')
-        self.sock.close()
+        sock.close()
+        sock = None
         return b'Configuration OK' in reply
 
     def reconfigure(self, config_path):
-        self.sock.connect(self.path)
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(self.path)
         message = bytes(f'configure "{config_path}" \n','utf-8')
         print(f'send message: {message}')
-        self.sock.sendall(message)
-        reply = self.sock.recv(1024)
+        sock.send(message)
+        reply = self.read_lines(3, sock)
         print(f'get reply: {reply}')
-        self.sock.close()
+        sock.close()
         return b'Reconfigured' in reply
 
 
